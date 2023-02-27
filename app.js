a5098e7699cfd555ae05fe8e2a0af4dfd39a4681
edouard <edouard.gugenheim@gmail.com>
2023-02-27 10:48:14 +0100

    creation of jobs model

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,7 +10,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const cors = require ('cors');
+const mongoose = require("mongoose");
 
+app.use (cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ models/jobs.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const jobSchema = mongoose.Schema({
+  job: { type: mongoose.Schema.Types.ObjectId, ref: "jobs" },
+  title: String,
+  description: { type: mongoose.Schema.Types.ObjectId, ref: "templates" },
+  contract: { type: mongoose.Schema.Types.ObjectId, ref: "contract" },
+  date: Date,
+  reference: String,
+  store: { type: mongoose.Schema.Types.ObjectId, ref: "stores" },
+  job_type: { type: mongoose.Schema.Types.ObjectId, ref: "job_types" },
+  is_top_offer: Boolean,
+  is_validated: Boolean,
+  candidate_found: Boolean,
+  is_displayed: Boolean,
+});
+
+const Job = mongoose.model("jobs", jobSchema);
+
+module.exports = Job;
