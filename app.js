e302a08145ddb4baa7135c338f6d33e49553df76
Sonia Kozak <kozaksonia@hotmail.fr>
2023-02-27 15:24:17 +0100

    route signIn, signUp Admin

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ require("./models/connection");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-
+const adminRouter = require("./routes/admin");
+var app = express(); 
 const cors = require("cors");
 app.use(cors());
 
@@ -22,5 +23,5 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
+app.use ("/admin", adminRouter);
 module.exports = app;
