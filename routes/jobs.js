295dbd8d97a916907a8309a879e35782b3162fa6
edouard <edouard.gugenheim@gmail.com>
2023-02-27 15:18:38 +0100

    update router get

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,73 +3,78 @@ var router = express.Router();
 const { checkBody } = require("../modules/checkBody");
 const Job = require("../models/jobs");
 
-// creation job advertisement
+// creat a new job advertisement
 router.post("/", (req, res) => {
   console.log(req.body);
   if (!checkBody(req.body, ["title"])) {
     res.json({ result: false, error: "Missing or empty field" });
     return;
   }
-  // check if job is already registred
+  // check if advertisement job is already registred
   Job.findOne({ reference: req.body.reference }).then((data) => {
     if (data === null) {
-      // jobis not yet registred => creat new job advertisement
+      // job not yet registred => creat new job advertisement
       const newJob = new Job({
         title: req.body.title,
         date: new Date(),
         reference: req.body.reference,
+        description: req.body.description,
+        contract: req.body.contract,
+        store:req.body.store,
+        job_type: req.body.job_type,
+        is_top_offer: false,
+        is_validated: false,
+        candidate_found: false,
+        is_displayed: false,
       });
-      // save the new user
+      // save the new  job advertisement
       newJob.save().then(() => {
-        res.json({ result: true, reference:newJob.reference});
+        res.json({ result: true, newjob: newJob });
       });
     } else {
-      // user is already registered
+      // job advertisement is already registered
       res.json({ result: false, error: "job advertisement  already exists" });
     }
   });
 });
 
-router.get("/:reference", (req, res) => {
-  Job.findOne({
-    reference: req.params.reference,
-  }).then((data) => {
-    if (data) { console.log(data);
-      res.json({ result: true, reference: data.reference });
+router.get("/", (req, res) => {
+  Job.find().then((data) => {
+    if (data) {
+      console.log(data);
+      res.json({ result: true, job: data });
     } else {
-      res.json({ result: false, error: "job not found" });
+      res.json({ result: false, error: "job advertisement not found" });
     }
   });
 });
 
-// router.put("/:reference", (req, res) => {
-//     Job.findOne({
-//       reference: req.body.reference,
-//     }).then((data) => {
-//       if (data) { console.log(data);
-//         Job.updateOne(
-
-//         )
-//         res.json({ result: true, reference: data.reference });
-//       } else {
-//         res.json({ result: false, error: "job not found" });
-//       }
-//     });
-//   });
-
-router.delete("/:delete", (req, res) => {
-    Job.deleteOne({
-      _id: req.params.delete,
+router.put("/:reference", (req, res) => {
+    Job.findOne({
+      reference: req.body.reference,
     }).then((data) => {
-      if (data.deletedCount>0) {
-        console.log(data);
-        res.json({ result: true, data: data.reference });
+      if (data) { console.log(data);
+        Job.updateOne(
+
+        )
+        res.json({ result: true, reference: data.reference });
       } else {
         res.json({ result: false, error: "job not found" });
       }
     });
   });
-  
 
+router.delete("/:delete", (req, res) => {
+  Job.deleteOne({
+    _id: req.params.delete,
+  }).then((data) => {
+    if (data.deletedCount > 0) {
+      console.log(data);
+      res.json({ result: true, data: data.reference });
+    } else {
+      res.json({ result: false, error: "job not found" });
+    }
+  });
+});
 
 module.exports = router;
